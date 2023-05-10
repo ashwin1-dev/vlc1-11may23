ad38049dd6ac27faeadbc66c6b04f2944b741ce3
ashwin1-dev <ashwin.devop12@gmail.com>
2023-05-10 12:41:29 +0000

    1st jar example

---
+++ src/main/java/com/tux/st1/App.java
@@ -0,0 +1,15 @@
+package com.tux.st1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Mumbai!" );
+        System.out.println( "Hello pune" );
+        System.out.println( "Hello nagpur" );
+    }
+}
